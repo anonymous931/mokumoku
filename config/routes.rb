43bc50f15baa8dfbaf6355cbcb98eec0d805a3b7
anonymous931 <fal825lere@gmail.com>
2023-02-20 02:03:55 +0900

    Add:フォロー機能やHobby,ProfileのViewの実装 テストが通るようにViewやルーティングを調整

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
-  resources :users, only: %i[new create] do
+  resources :users, only: %i[new create show] do
     resource :relationships, only: [:create, :destroy]
     get 'relationshops/followings'
     get 'relationshops/followers'
